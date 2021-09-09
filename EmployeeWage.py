bcbd681cfa0e74b6e7192290110534872d87ed65
omipapal12 <omipapal12@gmail.com>
2021-09-09 15:16:20 +0530

    Added: Employee Wage For A Month

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -1,41 +1,17 @@
 import random
 print("Welcome To Employee Wage Computation Program")
 
-# UC4:- Solving using Switch Case Statement
-randomcheck = random.randint(0, 9)
-IS_PRESENT = 1
-PART_TIME_HR = 4
+# UC5:- Calculating Wages For Month
+randomcheck = random.randint(0,9)
+IS_PRESENT = 0
 FULL_TIME_HR = 8
+PART_TIME_HR = 4
 WAGE_PER_HR = 20
+MONTHLY_WORKING_DAYS = 20
 
-
-def isPresentOrAbsent():
-    return "Employee Is Present"  if randomcheck % 2 == 1 else "Employee Is Absent"
-
-
-def isPartTime():
-    return "Employee Part Time Wage Is:" +str(PART_TIME_HR * WAGE_PER_HR)
-
-
-def isFullTime():
-    return "Employee Full Time Wage Is:" +str(FULL_TIME_HR * WAGE_PER_HR)
-
-
-def default():
-    return "Incorrect Choice"
-
-
-switcher = {
-    1: isPresentOrAbsent,
-    2: isPartTime,
-    3: isFullTime
-}
-
-
-def switch(employeewage):
-    return switcher.get(employeewage, default)()
-
-
-print(switch(1))
-print(switch(2))
-print(switch(3))
+if  randomcheck%2 == 0 :
+	print("Employee Is Full Time Worker")
+	print("The Monthly Wage Of Employee is: ",MONTHLY_WORKING_DAYS*WAGE_PER_HR*FULL_TIME_HR)
+else:
+	print("Employee Is Part Time Worker")
+	print("The Monthly Wage Of Employee Is: ",MONTHLY_WORKING_DAYS*WAGE_PER_HR*PART_TIME_HR)
