f64381140c7ebf010e7cc25a1bfb9ed196dc50be
omipapal12 <omipapal12@gmail.com>
2021-09-09 15:02:06 +0530

    Added: Daily Employee Wage

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -1,11 +1,14 @@
 import random
 print("Welcome To Employee Wage Computation Program")
 
-# UC1:- Check Employee Is Present Or Absent
-randomcheck = random.randint(0, 9)
+# UC2:- Calculate Daily Employee Wage
+randomcheck = random.randint(0,9)
 IS_PRESENT = 1
 IS_ABSENT = 0
-if randomcheck%2 == 1:
-	print("Employee Is Present" )
+FULL_DAY_HR = 8
+WAGE_PER_HR = 20
+if randomcheck%2 == 1 :
+	print("Employee Is Present")
+	print("Daily Employee Wage Is:" ,FULL_DAY_HR*WAGE_PER_HR)
 else :
-	print("Employee Is Absent")
+	print("Employee Is Absent So There Will be No Any Wage")
