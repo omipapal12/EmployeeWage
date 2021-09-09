3e372d385a33e4b3b5c96766908809ec2c9c1954
omipapal12 <omipapal12@gmail.com>
2021-09-09 15:05:06 +0530

    Added: Part Time Employee Wage

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -1,14 +1,17 @@
 import random
 print("Welcome To Employee Wage Computation Program")
 
-# UC2:- Calculate Daily Employee Wage
+#UC3:- Add Part Time Employee & Wage
 randomcheck = random.randint(0,9)
-IS_PRESENT = 1
-IS_ABSENT = 0
+IS_PART_TIME = 1
+IS_FULL_TIME = 2
 FULL_DAY_HR = 8
+PART_TIME_HR = 4
 WAGE_PER_HR = 20
-if randomcheck%2 == 1 :
-	print("Employee Is Present")
-	print("Daily Employee Wage Is:" ,FULL_DAY_HR*WAGE_PER_HR)
-else :
-	print("Employee Is Absent So There Will be No Any Wage")
+if randomcheck%2 == 1:
+	print("Employee Is Part Time")
+	print("So Employee Daily Part Time Wage Is: " ,PART_TIME_HR*WAGE_PER_HR)
+else:
+	print("Employee Is Full Time")
+	print("So Employee Daily Full Wage Is: " ,FULL_DAY_HR*WAGE_PER_HR)
+
