854cea4688e2c9af96d811d6d6c8252deae6cfcf
omipapal12 <omipapal12@gmail.com>
2021-09-09 14:58:38 +0530

    Added: Employee Is Present Or Absent

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -1,1 +1,11 @@
+import random
 print("Welcome To Employee Wage Computation Program")
+
+# UC1:- Check Employee Is Present Or Absent
+randomcheck = random.randint(0, 9)
+IS_PRESENT = 1
+IS_ABSENT = 0
+if randomcheck%2 == 1:
+	print("Employee Is Present" )
+else :
+	print("Employee Is Absent")
