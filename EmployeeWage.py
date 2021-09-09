1b6ef9b05d4671714d91b5a9b7d39717399f7004
omipapal12 <omipapal12@gmail.com>
2021-09-09 15:24:05 +0530

    Added: Employee Wage Till A Condition Of Total Working Hours Or Days Is Reached For A month

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -1,17 +1,17 @@
 import random
 print("Welcome To Employee Wage Computation Program")
 
-# UC5:- Calculating Wages For Month
+# UC6:- Calculate Wages till a condition of total working hours or days is reached for a month
+# Assume 100 hours and 20 days
 randomcheck = random.randint(0,9)
 IS_PRESENT = 0
 FULL_TIME_HR = 8
 PART_TIME_HR = 4
 WAGE_PER_HR = 20
 MONTHLY_WORKING_DAYS = 20
+WORKING_HR = 100
 
-if  randomcheck%2 == 0 :
-	print("Employee Is Full Time Worker")
-	print("The Monthly Wage Of Employee is: ",MONTHLY_WORKING_DAYS*WAGE_PER_HR*FULL_TIME_HR)
-else:
-	print("Employee Is Part Time Worker")
-	print("The Monthly Wage Of Employee Is: ",MONTHLY_WORKING_DAYS*WAGE_PER_HR*PART_TIME_HR)
+if randomcheck%2 == 0 and (WORKING_HR == 100 or MONTHLY_WORKING_DAYS):
+    print("Monthly Wage Is : " ,FULL_TIME_HR*WAGE_PER_HR*MONTHLY_WORKING_DAYS)
+else :
+    print("Part Time Wage:" ,PART_TIME_HR*WAGE_PER_HR*MONTHLY_WORKING_DAYS)
