137ac2ce385f0216a0541aba9cd58b1aaa12356b
omipapal12 <omipapal12@gmail.com>
2021-09-09 15:09:20 +0530

    Added: Employee Wage Using Dictionary

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -1,17 +1,41 @@
 import random
 print("Welcome To Employee Wage Computation Program")
 
-#UC3:- Add Part Time Employee & Wage
-randomcheck = random.randint(0,9)
-IS_PART_TIME = 1
-IS_FULL_TIME = 2
-FULL_DAY_HR = 8
+# UC4:- Solving using Switch Case Statement
+randomcheck = random.randint(0, 9)
+IS_PRESENT = 1
 PART_TIME_HR = 4
+FULL_TIME_HR = 8
 WAGE_PER_HR = 20
-if randomcheck%2 == 1:
-	print("Employee Is Part Time")
-	print("So Employee Daily Part Time Wage Is: " ,PART_TIME_HR*WAGE_PER_HR)
-else:
-	print("Employee Is Full Time")
-	print("So Employee Daily Full Wage Is: " ,FULL_DAY_HR*WAGE_PER_HR)
 
+
+def isPresentOrAbsent():
+    return "Employee Is Present"  if randomcheck % 2 == 1 else "Employee Is Absent"
+
+
+def isPartTime():
+    return "Employee Part Time Wage Is:" +str(PART_TIME_HR * WAGE_PER_HR)
+
+
+def isFullTime():
+    return "Employee Full Time Wage Is:" +str(FULL_TIME_HR * WAGE_PER_HR)
+
+
+def default():
+    return "Incorrect Choice"
+
+
+switcher = {
+    1: isPresentOrAbsent,
+    2: isPartTime,
+    3: isFullTime
+}
+
+
+def switch(employeewage):
+    return switcher.get(employeewage, default)()
+
+
+print(switch(1))
+print(switch(2))
+print(switch(3))
