5ee5daf713ea2b9084eecf4de7fd53acf8afea58
omipapal12 <omipapal12@gmail.com>
2021-09-13 13:45:09 +0530

    Added: Employee Wage Using Exceptions

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -1,7 +1,9 @@
 import random
+from py_compile import main
+
 print("Welcome To Employee Wage Computation Program")
 
-# UC7:- Using Method
+# UC8:- Using Exceptions
 
 IS_ABSENT = 0
 IS_PRESENT_FULL_DAY = 1
@@ -12,24 +14,34 @@ PART_TIME_HOURS = 4
 TOTAL_WORKING_DAYS = 20
 TOTAL_WORKING_HOURS = 100
 
-def calculateEmployeeWage():
-    empAttendence = {
-        IS_PRESENT_FULL_DAY : FULL_DAY_HOURS,
+def wage_calculate():
+    emp_attendence = {
+        IS_PRESENT_FULL_DAY: FULL_DAY_HOURS,
         IS_PRESENT_PART_TIME: PART_TIME_HOURS,
         IS_ABSENT: 0
-        }
+    }
 
-    workingDays = 0
-    totalWorkingHours = 0
+    if __name__ == "__main__":
+        main()
+        counter = 0
+        working_days = 0
+        total_working_hours = 0
+        #check_emp = random.randint(0, 2)
+        while working_days < TOTAL_WORKING_DAYS and total_working_hours < TOTAL_WORKING_HOURS:
+            try:
+                attendence = int(input("Enter the number for  attendance \n0-For Absent \n1- For Is Present Full Day "
+                                       "\n2- For Is Present Part Time \n"))
+                total_working_hours = total_working_hours + emp_attendence.get(attendence)
+                working_days = working_days + 1
+                counter = counter + 1
 
-    while(workingDays < TOTAL_WORKING_DAYS and totalWorkingHours < TOTAL_WORKING_HOURS):
-        checkEmp = random.randint(0,2)
-        totalWorkingHours = totalWorkingHours + empAttendence.get(checkEmp)
-        workingDays = workingDays + 1
+            except ValueError:
+                print("Enter valid input")
 
-    return totalWorkingHours * WAGE_PER_HOUR
+            else:
+                print(f"Total Wage Of Days {counter} is = {WAGE_PER_HOUR * total_working_hours}")
 
-totalWage = calculateEmployeeWage()
-print("Employee total wage is ",totalWage)
+        return WAGE_PER_HOUR * total_working_hours
 
-
+total_wage = wage_calculate()
+print("Employee total wage is ", total_wage)
